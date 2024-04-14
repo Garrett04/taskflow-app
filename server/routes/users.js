69e682176f0fc4f28b2747e578ff78fd6d1fc236
garrett04 <garrettcorreia21@gmail.com>
2024-04-14 16:57:35 +0100

    Add users route

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,23 @@
 const router = require('express').Router();
+const User = require('../models/User');
 const { isAuthenticated } = require('./middlewares/authMiddleware');
 
 // GET ROUTES
-router.get('/:id', isAuthenticated, (req, res) => {
-    res.send("Hello from protected route" + req.user.id);
+router.get('/', isAuthenticated, async (req, res) => {
+    const userId = req.user.id;
+    
+    const user = await User.findById(userId);
+
+    res.json({
+        success: true,
+        user: {
+            id: user.id,
+            username: user.username,
+            first_name: user.first_name,
+            last_name: user.last_name,
+            login_method: user.login_method
+        }
+    })
 })
 
 module.exports = router;
