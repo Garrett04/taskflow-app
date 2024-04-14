f1360681dc3c8c3f65b659a95286510ea51d3876
garrett04 <garrettcorreia21@gmail.com>
2024-04-14 17:52:58 +0100

    Setup users route complete

--- server/models/User.js
+++ server/models/User.js
@@ -66,6 +66,38 @@ class User {
             throw new Error(err);
         }
     }
+
+    async update(data) {
+        let statement = 'UPDATE users SET ';
+        let values = [data.id];
+
+        try {
+            // query statement
+            if (data.first_name && data.last_name) {
+                statement += 'first_name = $2, last_name = $3';
+                values.push(data.first_name, data.last_name);
+            } else if (data.first_name) {
+                statement += 'first_name = $2';
+                values.push(data.first_name);
+            } else if (data.last_name) {
+                statement += 'last_name = $2';
+                values.push(data.last_name);
+            }
+
+            statement += ' WHERE id = $1 RETURNING id, username, first_name, last_name';
+
+            // query database
+            const result = await db.query(statement, values);
+
+            if (result.rows.length > 0) {
+                return result.rows[0];
+            }
+
+            return null;
+        } catch (err) {
+            throw new Error(err);
+        }
+    }
 }
 
 module.exports = new User();
--- server/routes/users.js
+++ server/routes/users.js
@@ -3,6 +3,7 @@ const User = require('../models/User');
 const { isAuthenticated } = require('./middlewares/authMiddleware');
 
 // GET ROUTES
+// get a user by id
 router.get('/', isAuthenticated, async (req, res) => {
     const userId = req.user.id;
     
@@ -17,6 +18,24 @@ router.get('/', isAuthenticated, async (req, res) => {
             last_name: user.last_name,
             login_method: user.login_method
         }
+    })
+})
+
+// PUT ROUTES
+// update a user by id
+router.put('/', isAuthenticated, async (req, res) => {
+    const { first_name, last_name } = req.body;
+    const userId = req.user.id;
+
+    if (!first_name && !last_name) {
+        return res.status(404).json({ success: false, msg: "Please provide first_name/last_name to update user info" });
+    }
+
+    const updatedUser = await User.update({ id: userId, first_name, last_name });
+
+    res.json({ 
+        success: true,
+        user: updatedUser
     })
 })
 
