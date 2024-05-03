332d1f9c8e9fe9ab67178dc30754a0e508f2b6f8
garrett04 <garrettcorreia21@gmail.com>
2024-05-03 18:25:57 +0100

    Comment out cronJob.js

--- server/config/cronJob.js
+++ server/config/cronJob.js
@@ -1,57 +1,57 @@
-const cron = require('node-cron');
-const Task = require('../models/Task');
-const { isAfter, differenceInDays } = require('date-fns');
+// const cron = require('node-cron');
+// const Task = require('../models/Task');
+// const { isAfter, differenceInDays } = require('date-fns');
 
-// CRON SETUP
-// Updates the task status to overdue if current time exceeds the deadline date
-const updateTaskStatus = async () => {
-    const currentDate = new Date();
+// // CRON SETUP
+// // Updates the task status to overdue if current time exceeds the deadline date
+// const updateTaskStatus = async () => {
+//     const currentDate = new Date();
 
-    const tasks = await Task.find();
+//     const tasks = await Task.find();
 
-    tasks.forEach(
-        async ({id, deadline_date, status}) => {
-        if (isAfter(currentDate, deadline_date) && status === 'pending') {
-            // change status of task to overdue
-            const data = { id, status: "overdue" };
-            const updatedTask = await Task.update(data);
-            // console.log(updatedTask);
-            // console.log("deadline date:", deadline_date);
-            // console.log("current date:", currentDate);
-        }
-    })
-}
+//     tasks.forEach(
+//         async ({id, deadline_date, status}) => {
+//         if (isAfter(currentDate, deadline_date) && status === 'pending') {
+//             // change status of task to overdue
+//             const data = { id, status: "overdue" };
+//             const updatedTask = await Task.update(data);
+//             // console.log(updatedTask);
+//             // console.log("deadline date:", deadline_date);
+//             // console.log("current date:", currentDate);
+//         }
+//     })
+// }
 
-// Permanently deletes a task if the current date exceeds the deleted at date by 20 days.
-const deleteTask = async () => {
-    const currentDate = new Date();
+// // Permanently deletes a task if the current date exceeds the deleted at date by 20 days.
+// const deleteTask = async () => {
+//     const currentDate = new Date();
 
-    const tasks = await Task.find();
+//     const tasks = await Task.find();
 
-    tasks.forEach(
-        async ({ id, deleted_at, archived }) =>  {
-            if (archived) {
-                const difference = differenceInDays(currentDate, deleted_at);
+//     tasks.forEach(
+//         async ({ id, deleted_at, archived }) =>  {
+//             if (archived) {
+//                 const difference = differenceInDays(currentDate, deleted_at);
 
-                if (difference >= 30) {
-                    const deletedTask = await Task.deleteById(id);
-                    // console.log("deletedTask:", deletedTask);
-                    // console.log("deleted_at:", deleted_at);
-                }
-                // console.log("difference:", difference);
-            }
-        }
-    )
-}
+//                 if (difference >= 30) {
+//                     const deletedTask = await Task.deleteById(id);
+//                     // console.log("deletedTask:", deletedTask);
+//                     // console.log("deleted_at:", deleted_at);
+//                 }
+//                 // console.log("difference:", difference);
+//             }
+//         }
+//     )
+// }
 
-// For every 1 minute
-cronJob = cron.schedule("*/1 * * * *", async () => {
-    try {
-        await updateTaskStatus();
-        await deleteTask();
-    } catch (err) {
-        console.error(err);
-    }
-}) 
+// // For every 1 minute
+// cronJob = cron.schedule("*/1 * * * *", async () => {
+//     try {
+//         await updateTaskStatus();
+//         await deleteTask();
+//     } catch (err) {
+//         console.error(err);
+//     }
+// }) 
 
-module.exports = cronJob;
+// module.exports = cronJob;
